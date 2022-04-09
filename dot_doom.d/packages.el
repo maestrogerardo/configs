364f8a9d57f0621ac4dccb8cb2065f41283cf4a0
Gerhard A. Dittes <maestro.gerardo@gmail.com>
2022-04-09 15:26:32 +0200

    doom emacs: Install Debian related packages

--- dot_doom.d/packages.el
+++ dot_doom.d/packages.el
@@ -9,6 +9,9 @@
 ;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
 ;(package! some-package)
 
+(package! debian-el)
+(package! dpkg-dev-el)
+
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/raxod502/straight.el#the-recipe-format
