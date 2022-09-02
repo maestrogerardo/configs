17035b97988b9f59eb7ca70d1ae2dde4b4d2db91
Gerhard A. Dittes <maestro.gerardo@gmail.com>
2022-09-02 21:03:05 +0200

    dot_config/emacs/init.el: define custom "magit-commit-add-log-insert-function"

--- dot_config/emacs/init.el
+++ dot_config/emacs/init.el
@@ -13,3 +13,11 @@
 (add-hook 'c-mode-hook 'lsp)
 (add-hook 'c++-mode-hook 'lsp)
 (add-hook 'git-commit-setup-hook 'git-commit-turn-on-flyspell)
+
+;; custom "magit capital C"
+(setq magit-commit-add-log-insert-function 'gad_magit-commit-log-insert)
+
+(defun gad_magit-commit-log-insert (buffer file defun)
+  (with-current-buffer buffer
+    (goto-char (point-min))
+    (insert (format "%s: " file))))
