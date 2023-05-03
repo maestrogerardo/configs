84fa6748b141e65ecb931dc817a187649e283a33
Gerhard A. Dittes <gerhard.dittes@ionos.com>
2023-05-03 17:21:24 +0200

    dot_emacs.d/init.el ("ispell"): use English and German in parallel

--- dot_emacs.d/init.el
+++ dot_emacs.d/init.el
@@ -48,6 +48,13 @@
 (add-hook 'shell-dynamic-complete-functions
           'bash-completion-dynamic-complete)
 
+;; Use English and German in parallel for spell cheking
+(with-eval-after-load "ispell"
+	(setq ispell-program-name "hunspell")
+	(setq ispell-dictionary "en_US,de_DE")
+	(ispell-set-spellchecker-params)
+	(ispell-hunspell-add-multi-dic "en_US,de_DE"))
+
 ;; always use flycheck
 ;; (add-hook 'after-init-hook #'global-flycheck-mode)
 
