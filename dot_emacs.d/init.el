d2ce66cc33ca872ca1c897164483fa53f26b17a1
Gerhard A. Dittes <maestro.gerardo@gmail.com>
2023-05-22 16:04:46 +0200

    dot_emacs.d/init.el: use which-key

--- dot_emacs.d/init.el
+++ dot_emacs.d/init.el
@@ -17,6 +17,7 @@
 		tree-sitter-langs
 		treemacs
 		treemacs-magit
+		which-key
 		yascroll
 		))
 
@@ -32,6 +33,7 @@
 (tool-bar-mode 0)
 (scroll-bar-mode 1)
 (xterm-mouse-mode 1)
+(which-key-mode 1)
 
 ;; restore split winodows settings
 (winner-mode 1)
