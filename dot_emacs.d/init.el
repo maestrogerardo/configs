4021c2c9b455aa15b3891ad0f31cfcf66cb65e6c
Gerhard A. Dittes <maestro.gerardo@gmail.com>
2023-10-14 14:43:12 +0200

    dot_emacs.d/init.el: set auto-dim-other-buffers color to #f7f7f7

--- dot_emacs.d/init.el
+++ dot_emacs.d/init.el
@@ -209,7 +209,7 @@
 
 (custom-set-faces
  '(default ((t (:family "Noto Mono" :foundry "GOOG" :slant normal :weight regular :height 95 :width normal))))
- '(auto-dim-other-buffers-face ((t (:background "#f0f0f0"))))
+ '(auto-dim-other-buffers-face ((t (:background "#f7f7f7"))))
  '(whitespace-indentation ((t (:background "#ebebeb"))))
  '(whitespace-space-after-tab ((t (:background "#ebebeb"))))
  '(whitespace-space-before-tab ((t (:background "#ebebeb"))))
