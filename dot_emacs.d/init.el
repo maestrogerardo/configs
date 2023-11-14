c4d3c754394aa9345dfb4261bee309adef970a2a
Gerhard A. Dittes <gerhard.dittes@ionos.com>
2023-11-14 14:14:55 +0100

    emacs: extract "MELPA package stuff" (into separate file)

--- dot_emacs.d/init.el
+++ dot_emacs.d/init.el
@@ -1,41 +1,4 @@
-;; MELPA stuff
-(require 'package)
-(add-to-list 'package-archives '("MELPA Stable" . "https://stable.melpa.org/packages/") t)
-(package-initialize)
-(defvar gad-selected-packages)
-(setq gad-selected-packages
-      '(
-		auto-dim-other-buffers
-		bash-completion
-		company
-		debian-el
-		dpkg-dev-el
-		flycheck
-		go-mode
-		langtool
-		magit
-		php-mode
-		projectile
-		rainbow-mode
-		tree-sitter-langs
-		treemacs
-		treemacs-magit
-		vertico
-		web-mode
-		which-key
-		yascroll
-		))
-(defvar gad-selected-packages-lt-emacs-29)
-(setq gad-selected-packages-lt-emacs-29
-	  '(
-		eglot
-		tree-sitter
-		git-gutter
-		))
-
-(if (>= emacs-major-version 29)
-    (setq package-selected-packages gad-selected-packages)
-  (setq package-selected-packages (append gad-selected-packages gad-selected-packages-lt-emacs-29)))
+(load "/home/gad/dvl/src/github/configs/dot_emacs.d/lisp/gad-package.el")
 
 (setq inhibit-startup-screen t)
 (setq column-number-mode t)
