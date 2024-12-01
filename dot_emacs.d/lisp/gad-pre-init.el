3abaa01727ee00cab8af30a328a6549d67a1a9bb
Gerhard A. Dittes <maestro.gerardo@gmail.com>
2024-12-01 21:36:39 +0100

    dot_emacs.d/lisp/gad-pre-init.el: Enhance "prv" vs. "wrk" handling

--- dot_emacs.d/lisp/gad-pre-init.el
+++ dot_emacs.d/lisp/gad-pre-init.el
@@ -1,2 +1,17 @@
-(if (string-equal (getenv "GIT_AUTHOR_EMAIL") "gerhard.dittes@ionos.com")
-	(setq gad-work-setup t))
+(defun read-file-to-string (file-path)
+  "Read the contents of FILE-PATH and return as a string."
+  (with-temp-buffer
+	(insert-file-contents file-path)
+	(buffer-string)))
+
+(defun safe-file-content-equals (file-path comparison-string)
+  "Safely check if the contents of FILE-PATH are equal to COMPARISON-STRING. Return nil if the file doesn't exist."
+  (if (not (file-exists-p file-path))
+	  (progn (message "File does not exist: %s" file-path) nil)
+	(let ((file-content (string-trim (read-file-to-string file-path))))
+	  (string= file-content comparison-string))))
+
+(if (safe-file-content-equals "~/.gad_user_type" "wrk")
+	(progn (message (format "setup: wrk, email: %s" (getenv "GIT_AUTHOR_EMAIL")))
+		   (setq gad-work-setup t))
+  (message (format "setup: prv, email: %s" (getenv "GIT_AUTHOR_EMAIL"))))
