272df4e667ac7b8c2305bf2a72e55848af8ca2db
Gerhard A. Dittes <maestro.gerardo@gmail.com>
2024-12-02 10:51:45 +0100

    dot_emacs.d/lisp/gad-pre-init.el: Rename custom function

--- dot_emacs.d/lisp/gad-pre-init.el
+++ dot_emacs.d/lisp/gad-pre-init.el
@@ -4,7 +4,7 @@
 	(insert-file-contents file-path)
 	(buffer-string)))
 
-(defun gad-safe-file-content-equals (file-path comparison-string)
+(defun gad-file-content-equals (file-path comparison-string)
   "Safely check if the contents of FILE-PATH are equal to COMPARISON-STRING. Return nil if the file doesn't exist."
   (if (not (file-exists-p file-path))
 	  (progn (message "File does not exist: %s" file-path) nil)
@@ -17,7 +17,7 @@
 				(append mode-line-format (list (format " | %s" text))))
   (force-mode-line-update))
 
-(if (gad-safe-file-content-equals "~/.gad_user_type" "wrk")
+(if (gad-file-content-equals "~/.gad_user_type" "wrk")
 	(progn (message (format "setup: wrk, email: %s" (getenv "GIT_AUTHOR_EMAIL")))
 		   (gad-append-to-mode-line "wrk")
 		   (setq gad-work-setup t))
