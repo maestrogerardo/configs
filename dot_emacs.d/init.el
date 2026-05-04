eb3ade6d10f3a26c5963b25ebe02fcf1b3f8e0c7
Gerhard A. Dittes <maestro.gerardo@gmail.com>
2026-05-04 13:44:33 +0200

    dot_emacs.d: Migrate configuration to use-package
    Replace scattered setq/add-hook/require calls with use-package blocks.
    gad-package.el now only bootstraps package.el and use-package itself;
    package installation is handled via use-package-always-ensure t.

--- dot_emacs.d/init.el
+++ dot_emacs.d/init.el
@@ -5,81 +5,33 @@
 (setq custom-file "~/.emacs.d/lisp/custom.el")
 (load custom-file 'noerror 'nomessage)
 
-;; colors
+;; --- Core UI settings ---
 (set-background-color "#f0f0f0")
-
-;; start maximized
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
-
-(setq inhibit-startup-screen t)
-(setq column-number-mode t)
-(setq compilation-scroll-output t)
-(setq completion-styles '(orderless))
-
-;; backup in one place. flat, no tree structure
-(setq backup-directory-alist '(("" . "~/.emacs.d/backup")))
-
+(setq inhibit-startup-screen t
+      column-number-mode t
+      compilation-scroll-output t
+      backup-directory-alist '(("" . "~/.emacs.d/backup")))
 (setq-default fill-column 72)
 
-;; some modes for everywhere
 (menu-bar-mode 1)
 (tool-bar-mode 0)
 (scroll-bar-mode 0)
 (xterm-mouse-mode 1)
-(which-key-mode 1)
-(vertico-mode 1)
-(marginalia-mode 1)
-(global-diff-hl-mode 1)
-
-;; enable hl-line for all programming major modes and modes derived from text-mode
-;; kudos to https://emacsredux.com/blog/2020/11/21/disable-global-hl-line-mode-for-specific-modes/
-(add-hook 'prog-mode-hook #'hl-line-mode)
-(add-hook 'text-mode-hook #'hl-line-mode)
-
-;; Emacs >= 28
-(if (>= emacs-major-version 28) (context-menu-mode 1))
-
-;; Eamcs >= 29
-(if (>= emacs-major-version 29) (pixel-scroll-precision-mode 1))
-
-;; restore split windows settings
 (winner-mode 1)
-
-;; windmove
-(when (fboundp 'windmove-default-keybindings)
-  (windmove-default-keybindings))
+(when (>= emacs-major-version 28) (context-menu-mode 1))
+(when (>= emacs-major-version 29) (pixel-scroll-precision-mode 1))
+(when (fboundp 'windmove-default-keybindings) (windmove-default-keybindings))
 
 ;; e.g. switch cpp <-> hpp
 (global-set-key (kbd "C-<tab>") 'ff-find-other-file)
+(global-set-key (kbd "C-4") 'gad_recompile)
 
-;; dictionary
-(global-set-key (kbd "C-c l") #'dictionary-lookup-definition)
-(setq dictionary-server "dict.org")
-
-;; auto completion
-;; use company-box if running in graphical mode
-(if (display-graphic-p)
-    (progn (add-hook 'company-mode-hook 'company-box-mode)
-		   (setq company-box-frame-top-margin 20)))
-
-(global-company-mode 1)
-
-;; tree-sitter
-(global-tree-sitter-mode 1)
-(add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode)
-
-;; vterm
-(setq vterm-max-scrollback 100000)
-(setq vterm-kill-buffer-on-exit t)
-
-;; * * * C O D I N G   S T U F F * * *
-;; activate coding stuff automatically in most programming modes
-(add-hook 'prog-mode-hook #'gad_activateCodingStuff)
+;; --- User functions ---
 
 (defun gad_activateCodingStuff ()
-  ;; use real tabs
-  (setq-default indent-tabs-mode t)
-  (setq-default tab-width 4)
+  (setq-default indent-tabs-mode t
+                tab-width 4)
   (defvaralias 'c-basic-offset 'tab-width)
   (display-line-numbers-mode 1)
   (setq truncate-lines t)
@@ -92,104 +44,162 @@
   (add-hook 'eglot-managed-mode-hook (lambda () (eglot-inlay-hints-mode -1)))
   (copilot-mode 1))
 
-;; C, C++
-(add-hook 'c-mode-hook #'gad_c-and-cpp-mode-hook)
-(add-hook 'c++-mode-hook #'gad_c-and-cpp-mode-hook)
-
-;; Go
-(setq gofmt-command "goimports") ;; also add imports automatically
-(defun gad_go-mode-hook ()
-  ;; run gofmt before saving
-  (add-hook 'before-save-hook 'gofmt-before-save))
-(add-hook 'go-mode-hook 'gad_go-mode-hook)
-
-;; Shell
-(add-hook 'sh-mode-hook 'flycheck-mode)
-
-;; autoconf; automake; makefile, makefile-gmake
-(add-hook 'autoconf-mode-hook 'flycheck-mode)
-(add-hook 'makefile-automake-mode-hook 'flycheck-mode)
-(add-hook 'makefile-mode-hook 'flycheck-mode)
-(add-hook 'makefile-gmake-mode-hook 'flycheck-mode)
-
-;; recompile shortcut
-(global-set-key (kbd "C-4") 'gad_recompile)
 (defun gad_recompile ()
   "Interrupt current compilation and recompile."
   (interactive)
   (ignore-errors (kill-compilation))
   (recompile))
 
-;; Git
-(add-hook 'git-commit-setup-hook 'git-commit-turn-on-flyspell)
-
-;; Bash completion (eg. for "M-x shell")
-(autoload 'bash-completion-dynamic-complete
-  "bash-completion"
-  "BASH completion hook")
-(add-hook 'shell-dynamic-complete-functions 'bash-completion-dynamic-complete)
-
-;; org mode
-;;(setq org-log-done t)
-(eval-after-load "org" '(require 'ox-md nil t))
-(load "/home/gad/dvl/src/github/ox-jira.el/ox-jira.el" "missing-ok")
-
-;; Use English, German and Spanish in parallel for spell checking
-(ignore-errors
-  (with-eval-after-load "ispell"
-	(setq ispell-program-name "hunspell")
-	(setq ispell-dictionary "en_US,de_DE,es_ES")
-	(ispell-set-spellchecker-params)
-	(ispell-hunspell-add-multi-dic "en_US,de_DE,es_ES")))
-
-;; always use flycheck
-;; (add-hook 'after-init-hook #'global-flycheck-mode)
-
-;; custom "magit capital C"
 (defun gad_magit-commit-add-log-insert (buffer file defun)
   (with-current-buffer buffer
-	(magit-commit-add-log-insert buffer file defun)
-	(if defun
-		(cond ((re-search-backward (format "* %s (%s): " file defun) nil t)
-			   ;; (message (format "gad: buffer %s -> replacing \"* %s (%s): \"..." buffer file defun))
-			   (replace-match (format "%s (%s): " file defun))))
-	  (cond ((re-search-backward (format "* %s: " file) nil t)
-			 ;; (message (format "gad: buffer %s -> replacing \"* %s: \"..." buffer file))
-			 (replace-match (format "%s: " file)))))))
-(setq magit-commit-add-log-insert-function 'gad_magit-commit-add-log-insert)
+    (magit-commit-add-log-insert buffer file defun)
+    (if defun
+        (cond ((re-search-backward (format "* %s (%s): " file defun) nil t)
+               (replace-match (format "%s (%s): " file defun))))
+      (cond ((re-search-backward (format "* %s: " file) nil t)
+             (replace-match (format "%s: " file)))))))
 
-;; Whitespace stuff
-;; No marking, no extra handling of normal spaces, make tabs only slightly lighter
-(setq whitespace-style '(face trailing tabs newline empty indentation space-after-tab space-before-tab))
-(global-whitespace-mode 1)
-
-;; Disable WS mode for certain special modes
-(defun ab-enable-whitespace-mode () (not (derived-mode-p 'magit-mode)))
-(add-function :before-while whitespace-enable-predicate 'ab-enable-whitespace-mode)
-
-;; Potential use of copilot
-(require 'copilot)
-(define-key copilot-completion-map (kbd "<tab>") 'copilot-accept-completion)
-(define-key copilot-completion-map (kbd "TAB") 'copilot-accept-completion)
-
-;; mini-buildd support
-(setq mbd-archives '(ui))
-(load "/home/gad/dvl/src/salsa/mini-buildd/mini-buildd/share/emacs/site-lisp/mini-buildd-changelog-mode.el" "missing-ok")
-(load "/home/gad/dvl/src/salsa/mini-buildd/mini-buildd/share/emacs/site-lisp/mini-buildd-web-mode.el" "missing-ok")
-
-;; gb
 (defun gad-gb ()
   "Set up my preferred initial window layout for gb dev."
   (interactive)
   (delete-other-windows)
   (let ((main-dir "/home/gad/dvl/src/lundgren/gb/"))
-	(find-file main-dir)
-	(split-window-right)
-	(magit-status)
-	(split-window-below)
-	(other-window 1)
-	(vterm)
-	(treemacs)
-	(find-file "src/Main.cpp"))
+    (find-file main-dir)
+    (split-window-right)
+    (magit-status)
+    (split-window-below)
+    (other-window 1)
+    (vterm)
+    (treemacs)
+    (find-file "src/Main.cpp"))
   (let ((default-directory "/home/gad/dvl/src/lundgren/gb/"))
-	(compile "make -k -j$(nproc)")))
+    (compile "make -k -j$(nproc)")))
+
+;; --- Global hooks not tied to a specific package ---
+
+(add-hook 'prog-mode-hook #'gad_activateCodingStuff)
+(add-hook 'prog-mode-hook #'hl-line-mode)
+(add-hook 'text-mode-hook #'hl-line-mode)
+
+;; --- Packages ---
+
+(use-package which-key
+  :config (which-key-mode 1))
+
+(use-package vertico
+  :config (vertico-mode 1))
+
+(use-package marginalia
+  :config (marginalia-mode 1))
+
+(use-package orderless
+  :custom (completion-styles '(orderless)))
+
+(use-package diff-hl
+  :config (global-diff-hl-mode 1))
+
+(use-package company
+  :config (global-company-mode 1))
+
+(use-package company-box
+  :after company
+  :if (display-graphic-p)
+  :hook (company-mode . company-box-mode)
+  :custom (company-box-frame-top-margin 20))
+
+(use-package tree-sitter
+  :config
+  (global-tree-sitter-mode 1)
+  (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
+
+(use-package tree-sitter-langs
+  :after tree-sitter)
+
+(use-package vterm
+  :custom
+  (vterm-max-scrollback 100000)
+  (vterm-kill-buffer-on-exit t))
+
+(use-package flycheck
+  :hook ((sh-mode
+          autoconf-mode
+          makefile-automake-mode
+          makefile-mode
+          makefile-gmake-mode) . flycheck-mode))
+
+(use-package bash-completion
+  ;; shell-dynamic-complete-functions ends in -functions so -hook is not appended
+  :hook (shell-dynamic-complete-functions . bash-completion-dynamic-complete))
+
+(use-package go-mode
+  :custom (gofmt-command "goimports")
+  :hook (go-mode . (lambda ()
+                     (add-hook 'before-save-hook #'gofmt-before-save nil t))))
+
+(use-package cc-mode
+  :ensure nil
+  :hook
+  (c-mode   . gad_c-and-cpp-mode-hook)
+  (c++-mode . gad_c-and-cpp-mode-hook))
+
+(use-package magit
+  :custom (magit-commit-add-log-insert-function #'gad_magit-commit-add-log-insert)
+  :hook   (git-commit-setup . git-commit-turn-on-flyspell))
+
+(use-package copilot
+  :commands copilot-mode
+  :bind (:map copilot-completion-map
+              ("<tab>" . copilot-accept-completion)
+              ("TAB"   . copilot-accept-completion)))
+
+(use-package org
+  :ensure nil
+  :config
+  (require 'ox-md nil t)
+  (load "/home/gad/dvl/src/github/ox-jira.el/ox-jira.el" "missing-ok"))
+
+(use-package ispell
+  :ensure nil
+  :config
+  (ignore-errors
+    (setq ispell-program-name "hunspell"
+          ispell-dictionary "en_US,de_DE,es_ES")
+    (ispell-set-spellchecker-params)
+    (ispell-hunspell-add-multi-dic "en_US,de_DE,es_ES")))
+
+(use-package dictionary
+  :ensure nil
+  :bind    ("C-c l" . dictionary-lookup-definition)
+  :custom  (dictionary-server "dict.org"))
+
+(use-package whitespace
+  :ensure nil
+  :custom (whitespace-style '(face trailing tabs newline empty indentation
+                                   space-after-tab space-before-tab))
+  :config
+  (global-whitespace-mode 1)
+  (defun ab-enable-whitespace-mode ()
+    (not (derived-mode-p 'magit-mode)))
+  (add-function :before-while whitespace-enable-predicate #'ab-enable-whitespace-mode))
+
+;; Packages installed but not yet actively configured
+(use-package editorconfig)
+(use-package consult)
+(use-package projectile)
+(use-package treemacs)
+(use-package treemacs-magit :after (treemacs magit))
+(use-package yascroll)
+(use-package markdown-mode)
+(use-package php-mode)
+(use-package web-mode)
+(use-package rainbow-mode)
+(use-package rainbow-delimiters)
+(use-package debian-el)
+(use-package dpkg-dev-el)
+(use-package langtool)
+(use-package copilot-chat)
+
+;; mini-buildd support
+(setq mbd-archives '(ui))
+(load "/home/gad/dvl/src/salsa/mini-buildd/mini-buildd/share/emacs/site-lisp/mini-buildd-changelog-mode.el" "missing-ok")
+(load "/home/gad/dvl/src/salsa/mini-buildd/mini-buildd/share/emacs/site-lisp/mini-buildd-web-mode.el" "missing-ok")
--- dot_emacs.d/lisp/gad-package.el
+++ dot_emacs.d/lisp/gad-package.el
@@ -2,64 +2,10 @@
 (add-to-list 'package-archives '("MELPA" . "https://melpa.org/packages/") t)
 (package-initialize)
 
-;; default packages (emacs >= 30)
-(defvar gad-selected-packages)
-(setq gad-selected-packages
-      '(
-		bash-completion
-		company
-		company-box
-		consult
-		copilot
-		copilot-chat
-		debian-el
-		diff-hl
-		dpkg-dev-el
-		f
-		flycheck
-		go-mode
-		jsonrpc
-		langtool
-		magit
-		marginalia
-		markdown-mode
-		orderless
-		php-mode
-		projectile
-		rainbow-delimiters
-		rainbow-mode
-		request
-		transient
-		tree-sitter-langs
-		treemacs
-		treemacs-magit
-		vertico
-		vterm
-		web-mode
-		yascroll
-		))
+;; use-package is built-in since Emacs 29
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
 
-;; packages for emacs = 29
-(defvar gad-selected-packages-emacs-29)
-(setq gad-selected-packages-emacs-29-extra
-	  '(
-		editorconfig
-		which-key
-		))
-
-;; packages for emacs < 29
-(defvar gad-selected-packages-lt-emacs-29-extra)
-(setq gad-selected-packages-lt-emacs-29-extra
-	  '(
-		editorconfig
-		eglot
-		tree-sitter
-		which-key
-		))
-
-(if (>= emacs-major-version 30)
-    (setq package-selected-packages gad-selected-packages)
-  (if (= emacs-major-version 29)
-	  (setq package-selected-packages (append gad-selected-packages gad-selected-packages-emacs-29-extra))
-	(if (< emacs-major-version 29)
-		(setq package-selected-packages (append gad-selected-packages gad-selected-packages-lt-emacs-29-extra)))))
+(require 'use-package)
+(setq use-package-always-ensure t)
