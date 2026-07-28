95655a5dce1fd3ff3d9ce086ec74913dbac33535
Gerhard A. Dittes <gerhard.dittes@ionos.com>
2026-07-28 11:14:40 +0200

    dot_emacs.d/init.el (gad_go-mode-hook): Create

--- dot_emacs.d/init.el
+++ dot_emacs.d/init.el
@@ -57,6 +57,11 @@
   (add-hook 'eglot-managed-mode-hook (lambda () (eglot-inlay-hints-mode -1)))
   (ignore-errors (copilot-mode 1)))
 
+(defun gad_go-mode-hook ()
+  (eglot-ensure)
+  (add-hook 'before-save-hook #'gofmt-before-save nil t)
+  (ignore-errors (copilot-mode 1)))
+
 (defun gad_recompile ()
   "Interrupt current compilation and recompile."
   (interactive)
@@ -146,8 +151,8 @@
 
 (use-package go-mode
   :custom (gofmt-command "goimports")
-  :hook (go-mode . (lambda ()
-                     (add-hook 'before-save-hook #'gofmt-before-save nil t))))
+  :hook
+  (go-mode . gad_go-mode-hook))
 
 (use-package cc-mode
   :ensure nil
